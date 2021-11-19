4abf6af18b0ec8b48b9048c84de770520d3a4171
michael <michaelantuofermo@yahoo.com>
2021-11-18 20:34:16 -0500

    custom drawable

--- app/src/main/java/com/example/customdrawable/MainActivity.java
+++ app/src/main/java/com/example/customdrawable/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.customdrawable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll=findViewById(R.id.myMainLayout);
+
+        EditText et = new EditText(this);
+        Button btn = new Button(this);
+        LinearLayout.LayoutParams etParas=new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        et.setLayoutParams(etParas);
+        btn.setLayoutParams(etParas);
+        btn.setOnClickListener(e->{et.setText("Hello");});
+        btn.setText("click me");
+        ll.addView(et);
+        ll.addView(btn);
     }
 }
